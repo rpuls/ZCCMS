f3850e02e4972537235e35f574d014d6be1e2494
Rasmus <rasmuspuls@live.com>
2017-11-01 22:46:04 +0100

    issue 4 (colorpicker) completed- issue 5 (preview) started but svg's dont change color yet

--- admin/layout_editor.php
+++ admin/layout_editor.php
@@ -1,70 +1,111 @@
 <?php
 
 echo "<div id='sh0.5'>";
-echo "<div style='max-width:770px; margin:4px 4px 4px 4px; padding:5px; z-index:90; position:relative;' id='blue' class='visible-xsX alert alert-info'>";	
-echo "<div style='border-bottom:solid; border-bottom-width:1px; font-weight:bold;'>Webzite layout</div>";
-echo "<div class='row'>"; //row start
-//left panel (editor)
+	echo "<div style='max-width:770px; margin:4px 4px 4px 4px; padding:5px; z-index:90; position:relative;' id='blue' class='visible-xsX alert alert-info'>";	
+	echo "<div style='border-bottom:solid; border-bottom-width:1px; font-weight:bold;'>Webzite layout</div>";
+	
+	
+	
+	echo "<div class='row'>"; //row start
+	//left panel (editor)
 
-echo "<div class='col-md-6 col-xs-12'>";
-$svg_images = array_filter(scandir('../assets/svg'), function($item) {
-    if(pathinfo($item, PATHINFO_EXTENSION) == 'svg'){
-        return $item;
-    }
-});
-echo "<div style=' font-weight:bold;'>Chose edge style</div>";
-echo "<select id='svgSelect' class='form-control col-12'>";
-echo "<option>None (flat)</option>";
-foreach($svg_images as $svg) {
-    $svg = substr($svg, 0, -4);
-    echo "<option>$svg</option>";
-}
-echo "</select>";
-echo "</div>";
-//right panel (preview)
+	echo "<div class='col-md-6 col-xs-12'>";
+	$svg_images = array_filter(scandir('../assets/svg'), function($item) {
+		if(pathinfo($item, PATHINFO_EXTENSION) == 'svg'){
+			return $item;
+		}
+	});
+	echo "<div style=' font-weight:bold;'>Chose edge style</div>";
+	echo "<select id='svgSelect' class='form-control col-12'>";
+	echo "<option>None (flat)</option>";
+	foreach($svg_images as $svg) {
+		$svg = substr($svg, 0, -4);
+		echo "<option>$svg</option>";
+	}
+	echo "</select>";
+	echo "<input id='svgColor' type='color'>";
+	echo "</div>";
+	//right panel (preview)
 
-echo "<div class='col-md-6 col-xs-12'>";
-echo "dummy text to get a feel about the collumns bbbb bbbbbbbbbbb bbbb bbbbbbb bbbbbbbb bbbbb bbbbbbbb bbb bbbbb
-bbbbbb bbbbb bbbbbbb bbbb bbb bbbbbb bbbbbbb bbbbbbbbb bbbbbbbbbbb bbbb bbbbbbb bbbbbbbb bbbbb bbbbbbbb bbb bbbbb
-bbbbbb bbbbb bbbbbbb bbbb bbb bbbbbb bbbbbbb bbbbbbbbb bbbbbbbbbbb bbbb bbbbbbb bbbbbbbb bbbbb bbbbbbbb bbb bbbbb
-bbbbbb bbbbb bbbbbbb bbbb bbb bbbbbb bbbbbbb bbbbbbbbb bbbbbbbbbbb bbbb bbbbbbb bbbbbbbb bbbbb bbbbbbbb bbb bbbbb
-bbbbbb bbbbb bbbbbbb bbbb bbb bbbbbb bbbbbbb bbbbbbbbb bbbbbbbbbbb bbbb bbbbbbb bbbbbbbb bbbbb bbbbbbbb bbb bbbbb
-bbbbbb bbbbb bbbbbbb bbbb bbb bbbbbb bbbbbbb bbbbbbbbb bbbbbbbbbbb bbbb bbbbbbb bbbbbbbb bbbbb bbbbbbbb bbb bbbbb
-bbbbbb bbbbb bbbbbbb bbbb bbb bbbbbb bbbbbbb bbbbbbbbb bbbbbbbbbbb bbbb bbbbbbb bbbbbbbb bbbbb bbbbbbbb bbb bbbbb
-bbbbbb bbbbb bbbbbbb bbbb bbb bbbbbb bbbbbbb bbbbbbbbb bbbbbbbbbbb bbbb bbbbbbb bbbbbbbb bbbbb bbbbbbbb bbb bbbbb
-bbbbbb bbbbb bbbbbbb bbbb bbb bbbbbb bbbbbbb bbbbbbbbb bbbbbbbbbbb bbbb bbbbbbb bbbbbbbb bbbbb bbbbbbbb bbb bbbbb
-bbbbbb bbbbb bbbbbbb bbbb bbb bbbbbb bbbbbbb bbbbbbbbb bbbbbbbbbbb bbbb bbbbbbb bbbbbbbb bbbbb bbbbbbbb bbb bbbbb
-bbbbbb bbbbb bbbbbbb bbbb bbb bbbbbb bbbbbbb bbbbbbbbb bbbbbbbbbbb bbbb bbbbbbb bbbbbbbb bbbbb bbbbbbbb bbb bbbbb
-bbbbbb bbbbb bbbbbbb bbbb bbb bbbbbb bbbbbbb bbbbbbbbb bbbbbbbbbbb bbbb bbbbbbb bbbbbbbb bbbbb bbbbbbbb bbb bbbbb";
-echo "</div>";
+	echo "<div class='col-md-6 col-xs-12'>";
+	echo "
+		<style>
+			.section {
+				padding-top: 50px;
+				padding-bottom: 66px;
+				text-align-last: center;
+				color: white;
+				font-family: Arial, Helvetica, sans-serif;
+				display: inline-grid;
+			}
 
-echo "</div>";	//row end
-echo "<button id='ajaxTest'>ajax test</button><span id='ajaxResponse'></span>";
-echo "</div>";	//blue box div end
-echo "</div>\n"; //show/hide div end
+			.svg {
+				max-width: 100%;
+			}
 
-//javascript
-echo "
-<script>
-$('button#ajaxTest').click(function(){
-    var data = {}
-    data.action = 'layout';
-    data.svg = $('select#svgSelect').val();
-    console.log(data);
-    $('span#ajaxResponse').text('pending...');
-    $.ajax({
-        url: 'ajax_processor.php',
-        type: 'post',
-        data: data,
-        success: function(data){
-            $('span#ajaxResponse').text(data);
-        },
-        error: function (){
-            $('span#ajaxResponse').text('error :(');
-        }
-    });
-});
-</script>
-";
+			.svgBottom {
+				-ms-transform: rotate(180deg);
+				-webkit-transform: rotate(180deg); 
+				transform: rotate(180deg);
+			}
+
+		</style>
+		<div style=' font-weight:bold;'>Preview</div>
+		<div class='card'>
+			<div class='section'>
+				<img class='svg' src='../assets/svg/spikes2.svg'/>
+				
+				<div id='sectionDiv' style='background-color: red;'>
+					<h1>Section 2</h1>
+					<p>styled with svg's</p>
+				</div>
+
+				<img class='svg svgBottom' src='../assets/svg/spikes2.svg'/>
+			</div>
+		</div>
+	";
+	echo "</div>";
+
+	echo "</div>";	//row end
+	echo "<button id='ajaxTest'>ajax test</button><span id='ajaxResponse'></span>";
+	echo "</div>";	//blue box div end
+	echo "</div>\n"; //show/hide div end
+
+	//javascript
+	echo "
+	<script>
+	var fields = [];
+	$('.layoutField').each(function(i, obj) {
+		fields.push(obj);
+	});
+
+	$('select#svgSelect').on('change', function(){
+		$('img.svg').attr('src', '../assets/svg/'+this.value+'.svg');
+	});
+
+	$('input#svgColor').on('change', function(){
+		$('div#sectionDiv').css('background-color', this.value);
+		$('#path1').css({ fill: '#ffffff' });
+	});
+
+	$('button#ajaxTest').click(function(){
+		var data = {}
+		data.action = 'layout';
+		data.svg = $('select#svgSelect').val();
+		$('span#ajaxResponse').text('pending...');
+		$.ajax({
+			url: 'ajax_processor.php',
+			type: 'post',
+			data: data,
+			success: function(data){
+				$('span#ajaxResponse').text(data);
+			},
+			error: function (){
+				$('span#ajaxResponse').text('error :(');
+			}
+		});
+	});
+	</script>
+	";
 
 ?>
