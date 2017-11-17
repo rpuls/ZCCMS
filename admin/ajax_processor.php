b531284d075dec7db80028071c88c5e205735f4f
Rasmus <rasmuspuls@live.com>
2017-11-17 17:24:01 +0100

    issue 37 closed

--- admin/ajax_processor.php
+++ admin/ajax_processor.php
@@ -13,14 +13,26 @@ if($action == 'layout'){
             foreach($value as $se) {
                 $values[] = $se;
             }
+            $color1 = validateColor($values[1]);
+            $color2 = validateColor($values[2]);
+            $color3 = validateColor($values[3]);
+            
             $stmt = $mysqli_link->prepare("REPLACE INTO tblSvgElement(PlaceID, SVG, SVGPosition, Path1Color, Path2Color, Path3Color) VALUES (?,?,?,?,?,?)");
-            $stmt->bind_param("iissss", $zite_id, $values[0], $pos, $values[1], $values[2], $values[3]);
+            $stmt->bind_param("iissss", $zite_id, $values[0], $pos, $color1, $color2, $color3);
             $stmt->execute();
             $pos +=1;
         }
     }
     $stmt->close();
     $mysqli_link->close();
+}
+
+function validateColor($input){
+    $match = preg_match("/^(\#[\da-f]{3}|\#[\da-f]{6}|rgba\(((\d{1,2}|1\d\d|2([0-4]\d|5[0-5]))\s*,\s*){2}((\d{1,2}|1\d\d|2([0-4]\d|5[0-5]))\s*)(,\s*(0\.\d+|1))\)|hsla\(\s*((\d{1,2}|[1-2]\d{2}|3([0-5]\d|60)))\s*,\s*((\d{1,2}|100)\s*%)\s*,\s*((\d{1,2}|100)\s*%)(,\s*(0\.\d+|1))\)|rgb\(((\d{1,2}|1\d\d|2([0-4]\d|5[0-5]))\s*,\s*){2}((\d{1,2}|1\d\d|2([0-4]\d|5[0-5]))\s*)|hsl\(\s*((\d{1,2}|[1-2]\d{2}|3([0-5]\d|60)))\s*,\s*((\d{1,2}|100)\s*%)\s*,\s*((\d{1,2}|100)\s*%)\))$/",$input);
+    if(!$match){
+        return '#000000';
+    }
+    return $input;
 }
 
 ?>
